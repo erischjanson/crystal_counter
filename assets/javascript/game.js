537e62027640a8359eec4bce0167765c718e5a03
Elizabeth <erischjanson@gmail.com>
2018-01-01 11:40:13 -0600

    beurk

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,11 @@
-//webpage loads
-	//instructions
-	//random number displayed
-		//create random number
-		//display random number
-	//crystals displayed
-		//create random numbers
-		//link random numbers to crystals
-	//tracker for user's current score
-	//scoreboard with wins and losses displays but is empty
-		//comparing tracker with random number
-			//if it equals random number, game ends and wins increase
-			//if it goes over random number, game ends and losses increase
-			//if it is under random number, game continues
+$(document).ready(function(){
 
-
-//user will click on crystals until they either reach the random number or go over it
 
 var currentScore=0;
-var losses
-var wins
+var losses=0;
+var wins=0;
 var randomNumber = Math.floor(Math.random() * 100) + 1;
+$("#randomNumber").text(randomNumber);
 
 var gems = [ 
 	{
@@ -49,95 +35,82 @@ var gems = [
 		alt: "orange"
 	}
 ];
-console.log(gems[0].src);
-console.log(gems[0]);
 
 
+restartGame();
+//renderCrystals();
+
+function restartGame(){
+	alert("hi");
+	$("#currentScore").empty();
+	currentScore=0;
 	
-//calculate random numbers for crystals
-	for(var i = 0; i < gems.length; i++){
-		gems[i].value=Math.floor(Math.random() * 12) + 1;
+	$("#randomNumber").empty();
+	randomNumber = Math.floor(Math.random() * 100) + 1;
+	$("#randomNumber").text(randomNumber);
+	$("#jewel").empty();
+
+	renderCrystals();
+
 		
-		//gems[i].value=Math.floor(Math.random() * 12) + 1;
-	};
+}
 
 
 
-
-console.log(randomNumber);
-//function to render the crystals on the page
 function renderCrystals(){
+			for(var i = 0; i < gems.length; i++){
+		gems[i].value=Math.floor(Math.random() * 12) + 1;		
+	
+	}
+
 
 for(var i=0; i < gems.length; i++){
-	
+
 	//var imageCrystal=$("<img>").addClass("crystalImage");
 	var imageCrystal=$("<img>").addClass("crystalImage");	
 	imageCrystal.attr("src", gems[i].src);
 	imageCrystal.attr("value", gems[i].value);
 	console.log(gems[i].value);
+	console.log(imageCrystal);
 
 	//var imageCrystal=gems[i].src
 	$("#jewel").append(imageCrystal);
 	console.log(imageCrystal);
-};
+	
+}
 }
 
 
 
-//console.log(gems[0].src);
-/*function renderCrystals(){
-gems[0]=$("<img>").attr({
-	src: "assets/images/blue.png",
-	alt: "blue",
-	class: "crystalImage"
-});
-$("#jewel").append(gems[0]);
-
-gems[1]=$("<img>").attr({
-	src: "assets/images/pink.png",
-	alt: "pink",
-	class: "crystalImage"
-});
-$("#jewel").append(gems[1]);
-
-gems[2]=$("<img>").attr({
-	src: "assets/images/green.png",
-	alt: "green",
-	class: "crystalImage"
-});
-$("#jewel").append(gems[2]);
-
-gems[3]=$("<img>").attr({
-	src: "assets/images/orange.png",
-	alt: "orange",
-	class: "crystalImage"
-});
-$("#jewel").append(gems[3]);
-};
-*/
 
 
-//on-click events//
-//when crystal is clicked, add the points to the current score AND compare current score to random number;
+function checkWin(){
+	if(currentScore >  randomNumber){
+		alert("You lost");
+		losses++;
+		console.log(losses);		
+		restartGame();
+		//renderCrystals();
+	} if(currentScore==randomNumber){
+		alert("you win");
+		wins++;	
+		console.log(wins);	
+		restartGame();
+		//renderCrystals();
+	} 
+}
 
 
-
-console.log(gems[0].value);
-//gems[0].value=crystalNumber();
-
-
-
-
-$("#randomNumber").text(randomNumber);
-
-
-renderCrystals();
 
 $(".crystalImage").on("click", function() {
-	alert("working!" + $(this).attr("value"));
-	currentScore=currentScore + parseInt(($(this).attr("value")));
-	console.log(currentScore);
-	$("#currentScore").text(currentScore);
 	
+	currentScore=currentScore + parseInt(($(this).attr("value")));
+	$("#currentScore").text(currentScore);
+	console.log(currentScore);
+	
+	checkWin();
+})
 
 });
+
+//try getting rid of restart game function and just set variables etc as they should be in if statments
